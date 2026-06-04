4f13a3f8b0a46f212c51707c012ebfe82c4fa355
Roumya <roumyaranjan2@gmail.com>
2026-06-04 11:54:26 +0530

    Add Swagger openAPI documentation for all microservices

--- backend/auth-service/src/main/java/com/smartcommerce/authservice/config/SecurityConfig.java
+++ backend/auth-service/src/main/java/com/smartcommerce/authservice/config/SecurityConfig.java
@@ -28,7 +28,12 @@ public class SecurityConfig {
         .sessionManagement(session -> session
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/register", "/api/auth/login").permitAll()
+                .requestMatchers("/api/auth/**",
+                		"/api/auth/login",
+                		"/swagger-ui/**",
+                		"/swagger-ui.html",
+                		"/v3/api-docs/**"
+                		).permitAll()
                 .anyRequest().authenticated())
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
