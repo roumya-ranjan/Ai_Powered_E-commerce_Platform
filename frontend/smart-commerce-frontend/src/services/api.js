c88a218325628a2d823c1e30e38b9f5f9b30856d
Roumya <roumyaranjan2@gmail.com>
2026-06-12 20:53:09 +0530

    Update API gateway URL

--- frontend/smart-commerce-frontend/src/services/api.js
+++ frontend/smart-commerce-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://ai-powered-e-commerce-platform-api.onrender.com",
+    baseURL: "https://ai-powered-e-commerce-platform-api.onrender.com/api/products",
 });
 
 api.interceptors.request.use((config) => {
