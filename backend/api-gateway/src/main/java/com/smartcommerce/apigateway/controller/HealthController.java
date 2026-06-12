d26c248e3e8b25454df86829a188317f720e014e
Roumya <roumyaranjan2@gmail.com>
2026-06-12 12:30:29 +0530

    Fix health point

--- backend/api-gateway/src/main/java/com/smartcommerce/apigateway/controller/HealthController.java
+++ backend/api-gateway/src/main/java/com/smartcommerce/apigateway/controller/HealthController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HealthController {
 
-	@GetMapping("/helath")
+	@GetMapping("/health")
 	public String health() {
 		return "API Gateway Running";
 	}
